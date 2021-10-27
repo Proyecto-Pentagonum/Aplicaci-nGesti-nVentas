3ec140b919ed2e9b5748598c1f0391666b062488
Cabromacabro <oscarpedraza1@hotmail.com>
2021-10-27 16:24:17 -0500

    Ajustar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React, { useState,  useEffect } from 'react';
-import PublicLayout from 'layouts/PublicLayout';
 import PrivateLayout from 'layouts/PrivateLayout';
-import AuthLayout from 'layouts/AuthLayout';
-import Admin from 'pages/admin/Index';
+import PublicLayout from 'layouts/PublicLayout';
 import Index from 'pages/Index';
-import Login from 'pages/auth/Login';
-import Registro from 'pages/auth/Registro';
+import Admin from 'pages/admin/Index';
 import Productos from 'pages/admin/Productos';
-import Personas from 'pages/admin/Personas';
-import Ventas from 'pages/admin/Ventas';
+import Login from 'pages/auth/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'styles/styles.css';
+import Registro from 'pages/auth/Registro';
+import AuthLayout from 'layouts/AuthLayout';
 import { DarkModeContext } from 'context/darkMode';
+import Ventas from 'pages/admin/Ventas';
 import { Auth0Provider } from '@auth0/auth0-react';
+import Personas from 'pages/admin/Personas';
 import { UserContext } from 'context/userContext';
 import PrivateRoute from 'components/PrivateRoute';
 
@@ -31,61 +31,59 @@ function App() {
       audience='api-autenticacion-pentagonum'
     >
     <div className='App'>
-    <UserContext.Provider value={{ userData, setUserData }}>
-      <DarkModeContext.Provider value={{darkMode, setDarkMode}}>
-      <Router>
-      <Switch>
-        <Route path={['/admin', '/admin/productos', '/admin/ventas', '/admin/personas']}>
-          <PrivateLayout>
-            <Switch>
-              <Route path='/admin/productos'>
-              <PrivateRoute roleList={['admin']}>
-                <Productos />
-                </PrivateRoute>
-              </Route>
-              <Route path='/admin/ventas'>
-              <PrivateRoute roleList={['admin', 'vendedor']}>
-                <Ventas />
-              </PrivateRoute>
-              </Route>
-              <Route path='/admin/personas'>
-              <PrivateRoute roleList={['admin']}>
-                <Personas />
-              </PrivateRoute>
-              </Route>
-              <Route path='/admin'>
-                <Admin />
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <DarkModeContext.Provider value={{darkMode, setDarkMode}}>
+        <Router>
+          <Switch>
+            <Route path={['/admin', '/admin/productos', '/admin/ventas', '/admin/personas']}>
+              <PrivateLayout>
+                <Switch>
+                  <Route path='/admin/productos'>
+                    <PrivateRoute roleList={['admin']}>
+                      <Productos />
+                    </PrivateRoute>
+                  </Route>
+                  <Route path='/admin/ventas'>
+                    <PrivateRoute roleList={['admin', 'vendedor']}>
+                      <Ventas />
+                    </PrivateRoute>
+                  </Route>
+                  <Route path='/admin/personas'>
+                    <PrivateRoute roleList={['admin']}>
+                      <Personas />
+                    </PrivateRoute>
+                  </Route>
+                  <Route path='/admin'>
+                    <Admin />
+                  </Route>
+                </Switch>
+              </PrivateLayout>
+            </Route>
+            <Route path={['/login', '/registro']}>
+              <AuthLayout>
+                <Switch>
+                  <Route path='/login'>
+                    <Login />
+                  </Route>
+                  <Route path='/registro'>
+                    <Registro />
+                  </Route>
+                </Switch>
+              </AuthLayout>
+            </Route>
+            <Route path={['/']}>
+              <PublicLayout>
+                <Route path='/'>
+                  <Index />
+                </Route>
+                </PublicLayout>
               </Route>
             </Switch>
-          </PrivateLayout>
-        </Route>
-        <Route path={['/login', '/registro']}>
-          <AuthLayout>
-            <Switch>
-              <Route path='/login'>
-                <Login />
-              </Route>
-              <Route path='/registro'>
-                <Registro />
-              </Route>
-            </Switch>
-          </AuthLayout>
-        </Route>
-        <Route path={['/']}>
-          <PublicLayout>
-            <Switch>
-              <Route path='/'>
-                <Index />
-              </Route>
-            </Switch>
-          </PublicLayout>
-        </Route>
-      </Switch>
-    </Router>
-    </DarkModeContext.Provider>
-    </UserContext.Provider>
+          </Router>
+        </DarkModeContext.Provider>
+      </UserContext.Provider>
     </div>
-    </Auth0Provider>  
+  </Auth0Provider>  
   );
 }
 
